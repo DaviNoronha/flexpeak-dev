f774e43d6b04628980877b7e41300f1399c257db
Davi Noronha <davinoronha.g@gmail.com>
2022-01-19 14:47:06 -0400

    Autenticação, Bootstrap e Vue

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('{any}', function () {
     return view('app');
-})->where('any', '.*');
+})->where('any', '.*')->middleware('auth');
+
